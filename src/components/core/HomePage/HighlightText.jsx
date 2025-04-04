7bc76eacd518f0986297b8b3df398bbc76ef2d48
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-04 19:17:28 +0530

    Homepage updated till code animation

--- src/components/core/HomePage/HighlightText.jsx
+++ src/components/core/HomePage/HighlightText.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HighlightText = ({ text }) => {
     return (
-        <span className='font-bold text-richblue-200'>
-            {/* Try gradient to from as well in tailwind css */}
+        <span className='bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text font-bold'>
+            {/* gradient to from as well in tailwind css */}
             {" "}
             {text}
         </span>
