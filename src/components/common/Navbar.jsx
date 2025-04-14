1fddde3c1e960d4417cbac79df8fdd6ef7b08db2
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-14 22:31:02 +0530

    Navbar updated

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -5,8 +5,8 @@ import { NavbarLinks } from "../../data/navbar-links"
 
 const Navbar = () => {
     return (
-        <div>
-            <div>
+        <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
+            <div className='flex w-11/12 max-w-(--max-content) items-center justify-between'>
                 <Link to='/'>
                     <img src={logo} width={160} height={42} loading="lazy" />
                 </Link>
