1161261a341175dd385a229a4c05de70df9d45df
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-18 12:22:00 +0530

    cartSlice updated

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,7 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    totalItems: localStorage.getItem("totalItems") ? JSON.parse(localStorage.getItem("totalItems")) : 0,
+    // cart: localStorage.getItem("cart")
+    //     ? JSON.parse(localStorage.getItem("cart"))
+    //     : [],
+    // total: localStorage.getItem("total")
+    //     ? JSON.parse(localStorage.getItem("total"))
+    //     : 0,
+    totalItems: localStorage.getItem("totalItems")
+        ? JSON.parse(localStorage.getItem("totalItems"))
+        : 0,
 };
 
 const cartSlice = createSlice({
@@ -11,6 +19,13 @@ const cartSlice = createSlice({
         setTotalItems(state, value) {
             state.user = value.payload;
         },
+        // add to cart
+        // remove from cart
+        // reset cart
+        // addToCart: (state, value) => {
+        //     const course = action.payload;
+        //     const index = state.cart.findIndex((item) => item._id === course._id);
+        // }
     },
 });
 
