0f7c7f7618b209d5aa78a5e7281fc4ce999fe953
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-18 11:07:31 +0530

    Redux store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: rootReducer,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
