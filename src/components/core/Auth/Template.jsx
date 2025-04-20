038251825c0cf92ed0c5e5b0d6e1eed7a150f77e
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-20 21:30:49 +0530

    Template for login signup updated

--- src/components/core/Auth/Template.jsx
+++ src/components/core/Auth/Template.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import frameImg from "../../../assets/Images/frame.png"
+import LoginForm from "./LoginForm"
+import SignupForm from "./SignupForm"
 
 const Template = ({ title, description1, description2, image, formType }) => {
   const { loading } = useSelector((state) => state.auth);
@@ -9,8 +12,37 @@ const Template = ({ title, description1, description2, image, formType }) => {
       {loading ? (
         <div className='spinner'></div>
       ) : (
-        <div>
+        <div className='mx-auto flex w-11/12 max-w-(--max-content) md:mx-4'>
+          <div className='mx-auto w-11/12 max-w-[450px] md:mx-0'>
+            <h1 className='text-[1.8725rem] font-semibold leading-[2.375rem] text-richblack-5'>
+              {title}
+            </h1>
+            <p className='mt-4 text-[1.125rem] leading-[1.625rem]'>
+              <span className='text-richblack-100'>{description1}</span>
+              <span className='font-edu-sa font-bold italic text-blue-100'>
+                {description2}
+              </span>
+            </p>
+            {/* {formType === "signup" ? <SignupForm /> : <LoginFrom />} */}
+          </div>
 
+          <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0">
+            <img
+              src={frameImg}
+              alt="Pattern"
+              width={558}
+              height={504}
+              loading="lazy"
+            />
+            <img
+              src={image}
+              alt="Students"
+              width={558}
+              height={504}
+              loading="lazy"
+              className="absolute -top-4 right-4 z-10"
+            />
+          </div>
         </div>
       )}
     </div>
