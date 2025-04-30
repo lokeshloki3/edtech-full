21810b18ef9dfc88aff4bf3cd3565ea355740b01
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-30 15:56:42 +0530

    Quote in About page updated

--- src/components/core/AboutPage/Quote.jsx
+++ src/components/core/AboutPage/Quote.jsx
@@ -7,8 +7,9 @@ const Quote = () => {
             We are passionate about revolutionizing the way we learn. Our
             innovative platform
             <HighlightText text={"combines technology"} />
+            ,
             <span className="bg-gradient-to-b from-[#FF512F] to-[#F09819] text-transparent bg-clip-text font-bold">
-                ,{" "}
+                {" "}
                 expertise
             </span>
             , and community to create an
