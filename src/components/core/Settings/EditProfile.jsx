9a16f6058894010dfd2f29df868edbcf978584c0
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-09 15:56:51 +0530

    Edit Profile in settings added

---
+++ src/components/core/Settings/EditProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const EditProfile = () => {
+    return (
+        <div>
+            <form>
+                <div className="my-10 flex flex-col gap-y-6 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
+                    <h2 className="text-lg font-semibold text-richblack-5">
+                        Profile Information
+                    </h2>
+                    <div className='flex flex-col'>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default EditProfile
--- src/components/core/Settings/index.jsx
+++ src/components/core/Settings/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ChangeProfilePicture from './ChangeProfilePicture'
+import EditProfile from './EditProfile'
 
 const Settings = () => {
   return (
@@ -9,6 +10,8 @@ const Settings = () => {
       </h1>
 
       <ChangeProfilePicture />
+
+      <EditProfile />
     </div>
   )
 }
