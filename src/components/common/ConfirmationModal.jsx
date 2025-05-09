5d3282c2d7f3078f57a78018266e58aa81970428
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-09 12:40:33 +0530

    Opacity tailwind fixed and useRef added for logout modal

--- src/components/common/ConfirmationModal.jsx
+++ src/components/common/ConfirmationModal.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import IconBtn from "./IconBtn"
 
 const ConfirmationModal = ({ modalData }) => {
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                modalData?.btn2Handler(); // close the modal
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [modalData]);
+
     return (
-        <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
-            <div className="w-11/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6">
+        <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white/10 backdrop-blur-sm">
+            <div
+                ref={modalRef}
+                className="w-11/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6">
                 <p className="text-2xl font-semibold text-richblack-5">
                     {modalData?.text1}
                 </p>
