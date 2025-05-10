6d6834eb24f95f0f03e29ab3ed412bd4b4f0e774
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-11 01:14:17 +0530

    Cron job delete account fixed

--- server/jobs/deleteInactiveUsers.js
+++ server/jobs/deleteInactiveUsers.js
@@ -2,31 +2,24 @@ const cron = require("node-cron");
 const User = require("../models/User");
 const Profile = require("../models/Profile");
 const CourseProgress = require("../models/CourseProgress");
-const UserDeletionLog = require("../models/UserDeletionLog");
 
 // Schedule: every day at 1:00 AM
 exports.scheduleUserDeletionJob = () => {
     cron.schedule("0 1 * * *", async () => {
         const now = new Date();
+        // console.log("Current date for deletion check:", now);
         try {
+             // deletionScheduledAt timestamp is less than or equal to the current time.
             const usersToDelete = await User.find({
                 isDeleted: true,
-                deletionScheduledAt: { $lte: now }, // deletionScheduledAt timestamp is less than or equal to the current time.
+                deletionScheduledAt: { $lte: now },
             });
 
             for (const user of usersToDelete) {
                 await Profile.findByIdAndDelete(user.additionalDetails);
                 await CourseProgress.deleteMany({ userId: user._id });
-
-                // Log deletion
-                await UserDeletionLog.create({
-                    userId: user._id,
-                    email: user.email,
-                    reason: "Auto-deleted after 3 days of inactivity post deletion request",
-                });
-
                 await User.findByIdAndDelete(user._id);
-                console.log(`Deleted and logged user: ${user.email}`);
+                console.log(`Deleted user: ${user.email}`);
             }
 
             console.log("Daily cleanup completed.");
