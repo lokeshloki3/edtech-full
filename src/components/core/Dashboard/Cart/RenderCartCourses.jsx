9074c185c674808e947e3d8be1eebd50f2a0876f
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-12 13:02:21 +0530

    Enrolled courses for student updated

---
+++ src/components/core/Dashboard/Cart/RenderCartCourses.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RenderCartCourses = () => {
+  return (
+    <div>RenderCartCourses</div>
+  )
+}
+
+export default RenderCartCourses
--- src/components/core/Dashboard/EnrolledCourses.jsx
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import ProgressBar from "@ramonak/react-progress-bar";
+
+import { getUserEnrolledCourses } from "../../../services/operations/profileAPI";
 
 const EnrolledCourses = () => {
 
     const [enrolledCourses, setEnrolledCourses] = useState(null);
+    const { token } = useSelector((state) => state.auth);
+
+    const getEnrolledCourses = async () => {
+        try {
+            const response = await getUserEnrolledCourses(token);
+            setEnrolledCourses(response);
+        } catch (error) {
+            console.log("Unable to fetch Enrolled Courses");
+        }
+    }
+
+    useEffect(() => {
+        getEnrolledCourses();
+    }, []);
+
     return (
         <div>
             <div className='text-3xl text-richblack-50'>Enrolled Courses</div>
@@ -21,6 +40,31 @@ const EnrolledCourses = () => {
                         <p className='w-1/4 px-2 py-3'>Duration</p>
                         <p className='flex-1 px-2 py-3'>Progress</p>
                     </div>
+
+                    {enrolledCourses.map((course, index) => (
+                        <div key={index}>
+                            <div>
+                                <img src={course.thumbnail} />
+                                <div>
+                                    <p>{course.courseName}</p>
+                                    <p>{course.courseDescription}</p>
+                                </div>
+                            </div>
+
+                            <div>
+                                {course?.totalDuration}
+                            </div>
+
+                            <div>
+                                <p>Progress: {course.progressPercentage || 0}%</p>
+                                <ProgressBar
+                                    completed={course.progressPercentage || 0}
+                                    height="8px"
+                                    isLabelVisible={false}
+                                />
+                            </div>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
