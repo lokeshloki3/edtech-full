c51939bcaecddd4c5c88b73ffa8abfe041a6cfe2
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-16 12:35:34 +0530

    Course Builder form updated

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -2,17 +2,91 @@ import React, { useState } from 'react';
 import IconBtn from "../../../../common/IconBtn";
 import { useForm } from 'react-hook-form';
 import { IoAddCircleOutline } from "react-icons/io5";
+import { useDispatch, useSelector } from 'react-redux';
+import { MdNavigateNext } from "react-icons/md"
+import toast from 'react-hot-toast';
+import { setStep, setEditCourse, setCourse } from "../../../../../slices/courseSlice";
 
 const CourseBuilderForm = () => {
 
+  const { register, handleSubmit, setValue, formState: { errors }, } = useForm();
+
   const [loading, setLoading] = useState(false);
   const [editSectionName, setEditSectionName] = useState(null);
+  const { course } = useSelector((state) => state.course);
+  const dispatch = useDispatch();
 
-  const { register, handleSubmit, setValue, formState: { errors }, } = useForm();
+  const handleChangeEditSectionName = (sectionId, sectionName) => {
+    if (editSectionName === sectionId) {
+      cancelEdit();
+      return;
+    }
+    setEditSectionName(sectionId);
+    setValue("sectionName", sectionName);
+  }
+
+  const cancelEdit = () => {
+    setEditSectionName(null);
+    setValue("sectionName", "");
+  }
+
+  const goToNext = () => {
+    if (course.courseContent.length === 0) {
+      toast.error("Please add atleast one section");
+      return;
+    }
+    if (course.courseContent.some((section) => section.subSection.length === 0)) {
+      toast.error("Please add atleast one lecture in each section");
+      return;
+    }
+    dispatch(setStep(3));
+  }
+
+  const goBack = () => {
+    dispatch(setStep(1));
+    dispatch(setEditCourse(true));
+  }
+
+  const onSubmit = async (data) => {
+    // console.log("data", data);
+    setLoading(true);
+
+    let result
+
+    if (editSectionName) {
+      result = await updateSection(
+        {
+          sectionName: data.sectionName,
+          sectionId: editSectionName,
+          courseId: course._id,
+        },
+        token
+      )
+      // console.log("edit", result);
+    }
+    else {
+      result = await createSection(
+        {
+          sectionName: data.sectionName,
+          courseId: course._id,
+        },
+        token
+      )
+    }
+
+    if (result) {
+      // console.log("section result", result)
+      dispatch(setCourse(result));
+      setEditSectionName(null);
+      setValue("sectionName", "");
+    }
+    setLoading(false);
+  }
+
   return (
     <div className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
       <p className='text-2xl font-semibold text-richblack-5'>Course Builder</p>
-      <form className='space-y-4'>
+      <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
         <div className='flex flex-col space-y-2'>
           <label htmlFor="sectionName" className="text-sm text-richblack-5">
             Section Name <sup className="text-pink-200">*</sup>
@@ -42,6 +116,24 @@ const CourseBuilderForm = () => {
           </IconBtn>
         </div>
       </form>
+
+      {course.courseContent.length > 0 && (
+        <NestedView handleChangeEditSectionName={handleChangeEditSectionName} />
+      )}
+
+      {/* Next Prev Button */}
+      <div className='flex justify-end gap-x-3'>
+        <button
+          onClick={goBack}
+          className='flex cursor-pointer items-center gap-x-2 rounded-md bg-richblack-300 py-[8px] px-[20px] font-semibold text-richblack-900'
+        >
+          Back
+        </button>
+
+        <IconBtn disabled={loading} text="Next" onclick={goToNext}>
+          <MdNavigateNext />
+        </IconBtn>
+      </div>
     </div>
   )
 }
