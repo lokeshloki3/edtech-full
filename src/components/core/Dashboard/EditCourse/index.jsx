6fda0e632de799ea7f75beaed1af2427772d8abd
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-18 12:55:38 +0530

    Edit Course for Instructor updated

--- src/components/core/Dashboard/EditCourse/index.jsx
+++ src/components/core/Dashboard/EditCourse/index.jsx
@@ -36,14 +36,14 @@ const EditCourse = () => {
 
     return (
         <div>
-            <h1>
+            <h1 className="mb-14 text-3xl font-medium text-richblack-5">
                 Edit Course
             </h1>
-            <div>
+            <div className="mx-auto max-w-[600px]">
                 {course ? (
                     <RenderSteps />
                 ) : (
-                    <p>
+                    <p className="mt-14 text-center text-3xl font-semibold text-richblack-100">
                         Course not found
                     </p>
                 )}
