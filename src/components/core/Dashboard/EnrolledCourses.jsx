597220714747906c026febe2565a29777e008b8a
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-20 20:35:57 +0530

    Enrolled courses for student updated

--- src/components/core/Dashboard/EnrolledCourses.jsx
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -3,15 +3,26 @@ import { useSelector } from 'react-redux';
 import ProgressBar from "@ramonak/react-progress-bar";
 
 import { getUserEnrolledCourses } from "../../../services/operations/profileAPI";
+import { useNavigate } from 'react-router-dom';
 
 const EnrolledCourses = () => {
 
     const [enrolledCourses, setEnrolledCourses] = useState(null);
     const { token } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
 
     const getEnrolledCourses = async () => {
         try {
             const response = await getUserEnrolledCourses(token);
+
+            // Filtering the published course out
+            // const filterPublishCourse = res.filter((ele) => ele.status !== "Draft")
+            // console.log(
+            //   "Viewing all the couse that is Published",
+            //   filterPublishCourse
+            // )
+            // setEnrolledCourses(filterPublishCourse);
+
             setEnrolledCourses(response);
         } catch (error) {
             console.log("Unable to fetch Enrolled Courses");
@@ -41,21 +52,39 @@ const EnrolledCourses = () => {
                         <p className='flex-1 px-2 py-3'>Progress</p>
                     </div>
 
-                    {enrolledCourses.map((course, index) => (
-                        <div key={index}>
-                            <div>
-                                <img src={course.thumbnail} />
-                                <div>
-                                    <p>{course.courseName}</p>
-                                    <p>{course.courseDescription}</p>
+                    {enrolledCourses.map((course, index, arr) => (
+                        <div
+                            className={`flex items-center border border-richblack-700 ${index === arr.length - 1 ? "rounded-b-lg" : "rounded-none"}`}
+                            key={index}
+                        >
+                            <div
+                                className="flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3"
+                            // onClick={() => {
+                            //     navigate(
+                            //         `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`
+                            //     )
+                            // }}
+                            >
+                                <img
+                                    src={course.thumbnail}
+                                    alt="course_img"
+                                    className="h-14 w-14 rounded-lg object-cover"
+                                />
+                                <div className="flex max-w-xs flex-col gap-2">
+                                    <p className='font-semibold'>{course.courseName}</p>
+                                    <p className='text-sm text-richblack-300'>
+                                        {course.courseDescription.length > 50
+                                            ? `${course.courseDescription.slice(0, 50)}...`
+                                            : course.courseDescription}
+                                    </p>
                                 </div>
                             </div>
 
-                            <div>
+                            <div className="w-1/4 px-2 py-3">
                                 {course?.totalDuration}
                             </div>
 
-                            <div>
+                            <div className="flex w-1/5 flex-col gap-2 px-2 py-3">
                                 <p>Progress: {course.progressPercentage || 0}%</p>
                                 <ProgressBar
                                     completed={course.progressPercentage || 0}
