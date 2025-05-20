a8d75ebb8c651143ec9f74fb75c47ea6c3671861
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-20 16:16:57 +0530

    Payment frontend apis updated

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom';
+import { buyCourse } from '../services/operations/studentPaymentAPI';
 
 const CourseDetails = () => {
 
+    const { user } = useSelector((state) => state.profile);
+    const { token } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { courseId } = useParams();
+
     const handleBuyCourse = () => {
         // if user logged then only he can buy courses
-        // if (token) {
-        //     // handleBuyCourse()
-        // }
+        if (token) {
+            buyCourse(token, [courseId], user, navigate, dispatch);
+            return;
+        }
     }
     return (
         <div className='flex items-center'>
