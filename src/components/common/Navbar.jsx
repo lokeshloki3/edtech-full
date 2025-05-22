a9c57c06173edf2f2eec526a5c3c70c5894a3ea0
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-22 12:11:29 +0530

    Cart icon shows no. of items in Navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -63,10 +63,11 @@ const Navbar = () => {
     <div className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700
 			${location.pathname !== "/" ? "bg-richblack-800" : ""} transition-all duration-200`}>
       <div className='flex w-11/12 max-w-(--max-content) items-center justify-between'>
+        {/* Logo */}
         <Link to='/'>
           <img src={logo} width={160} height={32} loading="lazy" />
         </Link>
-
+        {/* Navigation Links */}
         <nav className="hidden md:block">
           <ul className="flex gap-x-6 text-richblack-25">
             {
@@ -134,11 +135,16 @@ const Navbar = () => {
         </nav>
 
         {/* Login/SignUp/Dashboard */}
-        <div className="flex gap-x-4 items-center">
+        <div className="hidden items-center gap-x-4 md:flex">
           {
             user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
               <Link to="/dashboard/cart" className="relative">
                 <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+                {totalItems > 0 && (
+                  <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-content-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                    {totalItems}
+                  </span>
+                )}
               </Link>
             )
           }
