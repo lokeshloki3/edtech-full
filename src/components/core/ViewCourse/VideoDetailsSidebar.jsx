334b5bf908cce1df81a9ec3e67cf14bf26e6c987
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-24 12:58:25 +0530

    ViewCourse sidebar css updated

--- src/components/core/ViewCourse/VideoDetailsSidebar.jsx
+++ src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import IconBtn from "../../common/IconBtn";
 import { useSelector } from 'react-redux';
+import { IoIosArrowBack } from "react-icons/io";
+import { BsChevronDown } from "react-icons/bs";
 
 const VideoDetailsSidebar = ({ setReviewModal }) => {
 
@@ -40,81 +42,95 @@ const VideoDetailsSidebar = ({ setReviewModal }) => {
 
   return (
     <>
-      <div className='text-white'>
+      <div className='flex h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800'>
         {/* For buttons and headings */}
-        <div>
+        <div className='flex flex-col items-start justify-between mx-5 gap-2 gap-y-4 border-b border-richblack-600 py-5 text-lg font-bold text-richblack-25'>
           {/* For buttons */}
-          <div>
+          <div className='flex w-full items-center justify-between'>
             <div
               onClick={() => {
                 navigate("/dashboard/enrolled-courses")
               }}
+              className="flex h-[35px] w-[35px] items-center justify-center rounded-full bg-richblack-100 p-1 text-richblack-700 hover:scale-90"
+              title='Back'
             >
-              Back
+              <IoIosArrowBack size={30} />
             </div>
 
-            <div>
-              <IconBtn
-                text="Add Review"
-                onclick={() => setReviewModal(true)}
-              />
-            </div>
+            <IconBtn
+              text="Add Review"
+              onclick={() => setReviewModal(true)}
+              customClasses="ml-auto"
+            />
           </div>
 
           {/* For heading and title */}
-          <div>
+          <div className='flex flex-col'>
             <p>{courseEntireData?.courseName}</p>
-            <p>{completedLectures?.length} / {totalNoOfLectures}</p>
+            <p className="text-sm font-semibold text-richblack-500">
+              {completedLectures?.length} / {totalNoOfLectures}
+            </p>
           </div>
         </div>
 
         {/* For sections and subsections */}
-        <div>
+        <div className="h-[calc(100vh - 5rem)] overflow-y-auto">
           {
             courseSectionData.map((section, index) => (
               <div
                 onClick={() => setActiveStatus(section?._id)}
                 key={index}
+                className='mt-2 cursor-pointer text-sm text-richblack-5'
               >
                 {/* Sections */}
-                <div>
-                  <div>
+                <div className='flex justify-between bg-richblack-600 px-5 py-4'>
+                  <div className="w-[65%] font-semibold">
                     {section?.sectionName}
+                  </div>
+                  <div className='flex items-center gap-3'>
+                    <span className="text-[12px] font-medium">
+                      {section?.subSection.length} Lesson(s)
+                    </span>
+                    <span
+                      className={`${activeStatus === section?._id
+                        ? "rotate-0"
+                        : "rotate-180"} transition-all duration-500`}
+                    >
+                      <BsChevronDown />
+                    </span>
                   </div>
                 </div>
 
                 {/* Subsections */}
-                <div>
-                  {
-                    activeStatus === section?._id && (
-                      <div>
-                        {
-                          section.subSection.map((topic, index) => (
-                            <div
-                              className={`flex gap-5 p-5 ${videoBarActive === topic._id
-                                ? "bg-yellow-200 text-richblack-900"
-                                : "bg-richblack-900 text-white"}`}
-                              key={index}
-                              onClick={() => {
-                                navigate(`/view-course/${courseEntireData?._id}/section/${section?._id}/sub-section/${topic?._id}`)
-                                setVideoBarActive(topic?._id);
-                              }}
-                            >
-                              <input
-                                type='checkbox'
-                                checked={completedLectures.includes(topic?._id)}
-                                onChange={() => { }}
-                              />
-                              <span>
-                                {topic.title}
-                              </span>
-                            </div>
-                          ))
-                        }
-                      </div>
-                    )
-                  }
-                </div>
+                {
+                  activeStatus === section?._id && (
+                    <div className="transition-all duration-500 ease-in-out">
+                      {
+                        section.subSection.map((topic, index) => (
+                          <div
+                            className={`flex gap-3 px-5 py-2 ${videoBarActive === topic._id
+                              ? "bg-yellow-200 text-richblack-900 font-semibold"
+                              : "hover:bg-richblack-900"}`}
+                            key={index}
+                            onClick={() => {
+                              navigate(`/view-course/${courseEntireData?._id}/section/${section?._id}/sub-section/${topic?._id}`)
+                              setVideoBarActive(topic?._id);
+                            }}
+                          >
+                            <input
+                              type='checkbox'
+                              checked={completedLectures.includes(topic?._id)}
+                              onChange={() => { }}
+                            />
+                            <span>
+                              {topic.title}
+                            </span>
+                          </div>
+                        ))
+                      }
+                    </div>
+                  )
+                }
               </div>
             ))
           }
