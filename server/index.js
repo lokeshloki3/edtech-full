9c6eb165506b8e035263d4d00bfd374387c35cb9
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-25 21:21:11 +0530

    CORS fixed for prod

--- server/index.js
+++ server/index.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        // origin: "http://localhost:5173",
+        origin: "https://studysphere-edtech.vercel.app",
         credentials: true,
     })
 )
