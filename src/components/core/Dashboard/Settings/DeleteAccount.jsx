4d26547ba0e7e1942b3d269c1ce5f8dd20970c6a
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-29 13:01:33 +0530

    Edit profile css updated

--- src/components/core/Dashboard/Settings/DeleteAccount.jsx
+++ src/components/core/Dashboard/Settings/DeleteAccount.jsx
@@ -22,16 +22,19 @@ const DeleteAccount = () => {
 
     return (
         <div>
-            <div className='my-10 flex gap-x-5 rounded-md border-[1px] border-pink-700 bg-pink-900 p-8 px-12'>
-                <div className='flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700'>
-                    <FiTrash2 className='text-3xl text-pink-200' />
-                </div>
-
-                <div className='flex flex-col space-y-2'>
+            <div className='my-10 flex flex-col gap-2 rounded-md border-[1px] border-pink-700 bg-pink-900 p-8 px-12'>
+                <div className='flex items-center gap-3'>
+                    <div className='flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700'>
+                        <FiTrash2 className='text-3xl text-pink-200' />
+                    </div>
                     <h2 className='text-lg font-semibold text-richblack-5'>
                         Delete Account
                     </h2>
-                    <div className='w-3/5 text-pink-25'>
+                </div>
+
+                <div className='flex flex-col space-y-2'>
+
+                    <div className='w-full text-pink-25'>
                         <p>Would you like to delete account?</p>
                         <p>
                             This account may contain Paid Courses. Deleting your account is
@@ -47,22 +50,24 @@ const DeleteAccount = () => {
                     >
                         I want to delete my account.
                     </button> */}
-                    <button
-                        type="button"
-                        className="w-fit cursor-pointer italic text-pink-300"
-                        onClick={() => {
-                            setConfirmationModal({
-                                text1: "Do you want to delete your account?",
-                                text2: "This action will permanently delete your account and all related data.",
-                                btn1Text: "Delete",
-                                btn2Text: "Cancel",
-                                btn1Handler: () => handleDeleteAccount(),
-                                btn2Handler: () => setConfirmationModal(null),
-                            });
-                        }}
-                    >
-                        I want to delete my account.
-                    </button>
+                    <div className='flex justify-end'>
+                        <button
+                            type="button"
+                            className="w-fit cursor-pointer italic text-pink-300"
+                            onClick={() => {
+                                setConfirmationModal({
+                                    text1: "Do you want to delete your account?",
+                                    text2: "This action will permanently delete your account and all related data.",
+                                    btn1Text: "Delete",
+                                    btn2Text: "Cancel",
+                                    btn1Handler: () => handleDeleteAccount(),
+                                    btn2Handler: () => setConfirmationModal(null),
+                                });
+                            }}
+                        >
+                            I want to delete my account.
+                        </button>
+                    </div>
                 </div>
             </div>
             {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
